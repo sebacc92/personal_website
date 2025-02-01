990482f7a6eb59b849f7b3f0e5758eec928c8f49
Seba Cardoso <hi@sebastiancardoso.com>
2025-02-01 00:27:47 -0300

    Refactor Contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -52,29 +52,7 @@ export default function Contact() {
             </div>
           </AnimatedSection>
 
-          <form name="contact" method="POST" data-netlify="true">
-            <p>
-              <label>Your Name: <input type="text" name="name" /></label>
-            </p>
-            <p>
-              <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-              <label>Your Role: <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select></label>
-            </p>
-            <p>
-              <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-              <button type="submit">Send</button>
-            </p>
-          </form>
-
-
-          {/* <motion.form
+          <motion.form
             name="contact"
             method="POST"
             // Opcional: agrega una página de éxito si la tienes creada
@@ -167,7 +145,7 @@ export default function Contact() {
               <Send className="h-4 w-4" />
               <span>{t.contact.send}</span>
             </motion.button>
-          </motion.form> */}
+          </motion.form>
         </div>
       </div>
     </section>
